eb884a90e32bcedc90424dbcee7ded35ac00185b
Arbaaz <khanarbaj45@.com>
2020-11-11 21:41:54 +0530

    DOCS: Update Login and Registration pages

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
-from .form import SignUpForm
+from .forms import SignUpForm, LoginForm
+from django.contrib import messages
+
 # Create your views here.
 def home(request):
     context = {'home' : 'active'}
@@ -11,12 +13,18 @@ def contact(request):
     return render(request, 'core/contact.html', context)
 
 def user_signup(request):
-    form = SignUpForm
-    return render(request, 'core/sign_up.html', {'form' :form})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Congratulation !! You have a become author.')
+            form.save()
+    else:
+        form = SignUpForm
+        return render(request, 'core/sign_up.html', {'form' :form})
 
 def user_login(request):
-    context = {'contact':'active'}
-    return render(request, 'core/login.html', context)
+    form = LoginForm()
+    return render(request, 'core/login.html', {'form' :form})
 
 def user_logout(request):
     return HttpResponseRedirect('/')
