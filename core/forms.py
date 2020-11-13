746adc92c607a1993dda21f02e56351d82bdff56
Arbaaz <khanarbaj45@.com>
2020-11-13 16:58:47 +0530

    DOCS: Added User Profile Information

--- core/forms.py
+++ core/forms.py
@@ -7,10 +7,10 @@ class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
-
+    desc = forms.CharField(required=False,       widget=forms.Textarea(attrs={'rows': 4, 'cols': 30}))
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'desc')
 
 # class SignUpForm(UserCreationForm):
 #     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-
+    desc = models.CharField(max_length=1000, blank=True)
     def __str__(self):
         return f'{self.user.username} Profile'
 
